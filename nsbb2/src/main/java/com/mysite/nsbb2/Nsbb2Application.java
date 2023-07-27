d4a5cf0b82dc58b4292bb08523978e8b3395b658
Namani <kwg531@naver.com>
2023-07-28 01:48:38 +0900

    Feat: `HelloLombok` 생성
    - `Gradle`에 devtools 추가
    - Chrome에 Live Reload 적용
    - Lombok 설치
    - `RequiredArgsContructor` 애노테이션 적용

---
+++ nsbb2/src/main/java/com/mysite/nsbb2/Nsbb2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.nsbb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nsbb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nsbb2Application.class, args);
+	}
+
+}
---
+++ nsbb2/src/test/java/com/mysite/nsbb2/Nsbb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.nsbb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nsbb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
